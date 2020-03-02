a4b2b0ae2dc927d57b10e4f86e0296a5b482c273
Anderson <anderson771218@gmail.com>
2020-03-02 23:33:41 +0800

    handle login and register(need to update urls)

--- src/components/login-signup-form/LoginSignupForm.jsx
+++ src/components/login-signup-form/LoginSignupForm.jsx
@@ -2,23 +2,72 @@ import React from "react";
 import { useState } from "react";
 import { Form, FormField, Button, Box, TextInput } from "grommet";
 import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import jwtDecoder from "jwt-decode";
 
 const ButtonGroup = styled(Box)`
   margin-top: 3rem;
 `;
 
 const LoginSignupForm = () => {
+  let history = useHistory();
   const [isLogin, setLogin] = useState(true);
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const resetFields = () => {
-    setName("");
     setEmail("");
     setPassword("");
   };
+
   const handleSubmit = event => {
     event.preventDefault();
+    if (isLogin) {
+      handleLogin();
+    } else {
+      handleRegister();
+    }
+  };
+  const handleLogin = async () => {
+    try {
+      const res = await fetch("http://localhost:3001/users/token", {
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({ email, password }),
+        method: "POST"
+      });
+      if (res.status !== 200) {
+        throw new Error("Login fail");
+      }
+      const token = await res.json();
+      localStorage.setItem("token", token);
+      const decoded = jwtDecoder(token);
+      if (decoded.role === "admin") {
+        history.push("/dashboard");
+      } else {
+        history.push("/chat");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handleRegister = async () => {
+    try {
+      const res = await fetch("http://localhost:3001/users", {
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({ email, password }),
+        method: "POST"
+      });
+      if (res.status !== 200) {
+        throw new Error("Register fail");
+      } else {
+        handleLogin();
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <Box
@@ -31,15 +80,6 @@ const LoginSignupForm = () => {
       elevation="medium"
     >
       <Form onSubmit={handleSubmit}>
-        {!isLogin && (
-          <FormField label="Name">
-            <TextInput
-              placeholder="placeholder"
-              value={name}
-              onChange={e => setName(e.target.value)}
-            />
-          </FormField>
-        )}
         <FormField label="Email">
           <TextInput
             placeholder="placeholder"
